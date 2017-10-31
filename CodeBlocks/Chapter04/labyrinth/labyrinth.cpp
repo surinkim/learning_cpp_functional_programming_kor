ed5741fe607777b2ffba57cb0aecec2770492c2d
hyunuk <nnhope@hotmail.com>
2017-10-31 22:21:08 +0900

    Update Chapter04 with labyrinth

--- CodeBlocks/Chapter04/labyrinth/labyrinth.cpp
+++ CodeBlocks/Chapter04/labyrinth/labyrinth.cpp
@@ -1,6 +1,7 @@
 /* labyrinth.cpp */
 #include <iostream>
 #include <vector>
+#include <unistd.h>
 
 using namespace std;
 
@@ -46,6 +47,89 @@ void displayLabyrinth(vector<vector<char>> labyrinth)
 	cout << "====================" << endl << endl;
 }
 
+bool navigate(
+	vector<vector<char>> labyrinth,
+	int row,
+	int col)
+{
+	// labyrinth 출력
+	displayLabyrinth(labyrinth);
+
+	cout << "Checking cell (";
+	cout << row << "," << col << ")" << endl;
+
+	// 미로 탐색 과정을 명확히 보여주기 위해
+	// 경로 탐색 전에 1초 대기한다.
+	sleep(1);
+
+	if (labyrinth[row][col] == 'F')
+	{
+		cout << "Yeayy.. ";
+		cout << "Found the finish flag ";
+		cout << "at point (" << row << ",";
+		cout << col << ")" << endl;
+		return (true);
+	}
+	else if (
+		labyrinth[row][col] == '#' ||
+		labyrinth[row][col] == '*')
+	{
+		return (false);
+	}
+	else if (labyrinth[row][col] == ' ')
+	{
+		labyrinth[row][col] = '*';
+	}
+
+	if ((row + 1 < rows) &&
+		navigate(labyrinth, row + 1, col))
+	return (true);
+
+	if ((col + 1 < cols) &&
+		navigate(labyrinth, row, col + 1))
+		return (true);
+
+	if ((row - 1 >= 0) &&
+		navigate(labyrinth, row - 1, col))
+		return (true);
+
+	if ((col - 1 >= 0) &&
+		navigate(labyrinth, row, col - 1))
+		return (true);
+
+	return (false);
+}
+
+bool isLabyrinthSolvable(
+	vector<vector<char>> labyrinth)
+{
+	int start_row = -1;
+	int start_col = -1;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			if (labyrinth[i][j] == 'S')
+			{
+				start_row = i;
+				start_col = j;
+				break;
+			}
+		}
+	}
+
+	if (start_row == -1 || start_col == -1)
+	{
+		cerr << "No valid starting point found!" << endl;
+		return (false);
+	}
+
+	cout << "Starting at point (" << start_row << ",";
+	cout << start_col << ")" << endl;
+
+	return navigate(labyrinth, start_row, start_col);
+}
+
 auto main() -> int
 {
 	vector<vector<char>> labyrinth = createLabyrinth();
@@ -55,6 +139,10 @@ auto main() -> int
 	cout << endl << "Press enter to continue..." << endl;
 	getline(cin, line);
 
+	if (isLabyrinthSolvable(labyrinth))
+		cout << "Labyrinth solved!" << endl;
+	else
+		cout << "Labyrinth could not be solved!" << endl;
+
 	return 0;
 }
-
