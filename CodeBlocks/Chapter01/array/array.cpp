3b93d0e6ab895627aeb71402893a3c66410b5837
hyunuk <nnhope@hotmail.com>
2017-11-14 23:45:19 +0900

    Review Chapter01

--- CodeBlocks/Chapter01/array/array.cpp
+++ CodeBlocks/Chapter01/array/array.cpp
@@ -9,18 +9,16 @@ auto main() -> int
 	// 10개의 정수로 배열 초기화
 	std::array<int, 10> arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	// 배열의 각 원소를 출력한다.
+	// 배열의 각 요소를 출력한다.
 	std::cout << "Original Data : ";
 	for (auto a : arr) std::cout << a << " ";
 	std::cout << std::endl;
 
-	// Modifying the content of
-	// the 1st and 3rd element of the array
-	// 배열의 첫 번째와 세 번째 원소에 새로운 값을 설정한다.
+	// 배열의 첫 번째와 세 번째 요소에 새로운 값을 설정한다.
 	arr[1] = 9;
 	arr[3] = 7;
 
-	// 수정된 배열의 원소를 출력한다.
+	// 수정된 배열의 요소를 출력한다.
 	std::cout << "Manipulated Data: ";
 	for (auto a : arr) std::cout << a << " ";
 	std::cout << std::endl;
--- CodeBlocks/Chapter01/begin_end/begin_end.cpp
+++ CodeBlocks/Chapter01/begin_end/begin_end.cpp
@@ -8,16 +8,16 @@ auto main() -> int
 	// 배열 선언
 	int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	// 전통적인 for-loop를 사용해서
-	// 배열 내의 원소를 출력한다.
+	// 보통의 for 루프를 사용해서
+	// 배열 내의 요소를 출력한다.
 	std::cout << "Displaying array element using conventional for-loop";
 	std::cout << std::endl;
 	for (unsigned int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
 		std::cout << arr[i] << " ";
 	std::cout << std::endl;
 
-	// 비-멤버 begin(), end() 함수를 사용해서
-	// 배열 내의 원소를 출력한다.
+	// 비멤버 begin(), end() 함수를 사용해서
+	// 배열 내의 요소를 출력한다.
 	std::cout << "Displaying array element using non-member begin() and end()";
 	std::cout << std::endl;
 	for (auto i = std::begin(arr); i != std::end(arr); ++i)사
