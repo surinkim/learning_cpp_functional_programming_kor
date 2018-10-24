99e08765e696f0963be2be6daea1cb4c5b17b0bb
김현욱C <hukim@smilegate.com>
2018-10-25 08:27:32 +0900

    Add Errata in README.md

--- CodeBlocks/Chapter01/range_based_for_loop/range_based_for_loop.cpp
+++ CodeBlocks/Chapter01/range_based_for_loop/range_based_for_loop.cpp
@@ -8,7 +8,7 @@ auto main() -> int
 	// 배열을 선언한다.
 	int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	// 비-멤버 begin(), end() 함수를 사용해서
+	// 범위 기반 for 루프를 사용해서
 	// 배열 내의 원소를 출력한다.
 	std::cout << "Displaying array element using range-based for loop";
 	std::cout << std::endl;
